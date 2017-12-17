da49a962a5b20b29d3d1b0026444bef91bd40035
cheficha <stefanzhekov95@gmail.com>
2017-12-17 15:45:48 +0200

    shipshipships

--- Battleships/Battleships/Models/Ship.cs
+++ Battleships/Battleships/Models/Ship.cs
@@ -7,46 +7,31 @@ using Battleships.Utilities.Contracts;
 
 namespace Battleships.Models
 {
-    public abstract class Ship : IShip, IShipElement
+    public abstract class Ship : IShip
     {
         private bool isAlive;
         private int health;
         private ShipType shipType;
-        private IPosition position;
-        private GameObjectElementType elementType;
         private Direction direction;
-        private bool isHit;
-        public Ship(Position position, Direction direction)
+        private IEnumerable<IGameObjectElement> element;
+        
+        public Ship(Direction direction,ShipType type)
         {
-            this.position = position;
             this.direction = direction;
-            this.elementType = GameObjectElementType.Ship;
             this.isAlive = true;
-            this.isHit = false;
+            
            
         }
 
 
-        public IPosition Position { get { return this.position; } set { this.position = value; } }
         public Direction Direction { get { return this.direction; } private set { this.direction = value; } }
 
 
 
         public bool IsAlive { get { return this.isAlive; } set {this.isAlive=value; } }
         public int Health { get { return this.health; } set {this.health=value; } }
-        public IGameObjectElement this[int index] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public IGameObjectElement this[int index] { get ; set; }
         public IEnumerable<IGameObjectElement> Elements { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public bool IsHit { get; set; }
-
-        
-
-        public GameObjectElementType Type => throw new NotImplementedException();
-
-        public void GetElementHit(IGameObjectElement hitGameObjectElement)
-        {
-            throw new NotImplementedException();
-        }
-
         public void GetHit()
         {
           
