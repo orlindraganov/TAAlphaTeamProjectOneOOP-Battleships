075c2073a4ef6be7a0edcaa244fad144db4af528
cheficha <stefanzhekov95@gmail.com>
2017-12-17 13:42:59 +0200

    Struct Position Created

--- Battleships/Battleships/Models/Contracts/IWater.cs
+++ Battleships/Battleships/Models/Contracts/IWater.cs
@@ -1,6 +1,12 @@
-﻿namespace Battleships.Models.Contracts
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleships.Models.Contracts
 {
-    public interface IWater : IGameObject
+    interface IWater:IGameObject
     {
     }
 }
