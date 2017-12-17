2bf09c90ff41228e407ab7bf3a6a8baf5a3b0381
cheficha <stefanzhekov95@gmail.com>
2017-12-17 13:10:19 +0200

    enumShipType

--- Battleships/Battleships/Enums/ShipType.cs
+++ Battleships/Battleships/Enums/ShipType.cs
@@ -1,11 +1,23 @@
-﻿namespace Battleships.Enums
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleships.Enums
 {
-    public enum ShipType
-    {
-        Carrier,
-        Battleship,
-        Cruiser,
-        Submarine,
-        Destroyer
+   
+   
+   
+    
+        public enum ShipType
+        {
+            Carrier,
+            Battleship,
+            Cruiser,
+            Submarine,
+            Destroyer
+        }
     }
-}
+
+
