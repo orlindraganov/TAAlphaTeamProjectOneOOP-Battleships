1633a9614715ec637f971ea2443eae542d82567e
orlindraganov <orlindraganov@gmail.com>
2017-12-18 15:21:56 +0200

    Minimal changes

--- Battleships/Battleships/Utilities/Position.cs
+++ Battleships/Battleships/Utilities/Position.cs
@@ -4,24 +4,13 @@ namespace Battleships.Utilities
 {
     public class Position : IPosition
     {
-
-        public Position(int x, int y) 
+        public Position(int x, int y)
         {
             this.X = x;
             this.Y = y;
         }
+
         public int X { get; set; }
         public int Y { get; set; }
-
-        //No methods in structs. We'll calculate this in the input parser 
-        //or we can tweek the matrix to be [A1] iteratable
-        //
-        //public Position GetFromMatrix(char row, int col)
-        //{
-        //    X = row - 65;
-        //    Y = col - 1;
-        //    var position = new Position(X, Y);
-        //    return position;
-        //}
     }
 }
