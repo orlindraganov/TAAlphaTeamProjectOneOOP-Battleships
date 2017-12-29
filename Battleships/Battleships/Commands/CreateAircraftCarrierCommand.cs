bf886f4987007ac9d664ced2a1e314dbe0ecc0f6
cheficha <stefanzhekov95@gmail.com>
2017-12-29 19:09:41 +0200

    Some commands created

---
+++ Battleships/Battleships/Commands/CreateAircraftCarrierCommand.cs
@@ -0,0 +1,54 @@
+﻿using Battleships.BattleShipsEngine;
+using Battleships.BattleShipsEngine.Contracts;
+using Battleships.Enums;
+using Battleships.Factory;
+using Battleships.Models.Contracts;
+using Battleships.Utilities.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Battleships.Utilities;
+
+namespace Battleships.Commands
+{
+    public class CreateAircraftCarrierCommand : ICommand
+    {
+        private readonly IBattleShipFactory factory;
+        private readonly IEngine engine;
+
+        public CreateAircraftCarrierCommand(IBattleShipFactory factory, IEngine engine)
+        {
+            this.factory = factory;
+            this.engine = engine;
+        }
+        public string Execute(IList<string> parameters)
+        {
+            int row;
+            int col;
+            Direction direction;
+            IPosition pos = new Position();
+           
+
+            try
+            {
+                row = int.Parse(parameters[0]);
+                col = int.Parse(parameters[1]);
+                direction = (Direction)Enum.Parse(typeof(Direction), parameters[2]);
+                pos.Row = row;
+                pos.Col = col;
+
+
+            }
+            catch 
+            {
+
+                throw new ArgumentException("Invalid parameters");
+            }
+            var AircraftCarrier = this.factory.CreateAircraftCarrier(pos,direction);
+            return  $"AircraftCarrier with position {pos} and direction {direction} was created ";
+        }
+
+    }
+}
---
+++ Battleships/Battleships/Commands/CreateFrigateCommand.cs
@@ -0,0 +1,52 @@
+﻿using Battleships.BattleShipsEngine;
+using Battleships.BattleShipsEngine.Contracts;
+using Battleships.Enums;
+using Battleships.Factory;
+using Battleships.Utilities;
+using Battleships.Utilities.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleships.Commands
+{
+    public class CreateFrigateCommand:ICommand
+    {
+        private readonly IBattleShipFactory factory;
+        private readonly IEngine engine;
+
+        public CreateFrigateCommand(IBattleShipFactory factory, IEngine engine)
+        {
+            this.factory = factory;
+            this.engine = engine;
+        }
+        public string Execute(IList<string> parameters)
+        {
+            int row;
+            int col;
+            Direction direction;
+            IPosition pos = new Position();
+
+
+            try
+            {
+                row = int.Parse(parameters[0]);
+                col = int.Parse(parameters[1]);
+                direction = (Direction)Enum.Parse(typeof(Direction), parameters[2]);
+                pos.Row = row;
+                pos.Col = col;
+
+
+            }
+            catch
+            {
+
+                throw new ArgumentException("Invalid parameters");
+            }
+            var AircraftCarrier = this.factory.CreateAircraftCarrier(pos, direction);
+            return $"Frigate with position {pos} and direction {direction} was created ";
+        }
+    }
+}
