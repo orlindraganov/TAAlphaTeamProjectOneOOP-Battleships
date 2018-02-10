8e921d453c228ccef3f182e455c2f1db60238950
simeonovanton <djnoreaga@gmail.com>
2018-02-10 18:03:24 +0200

    Making Unit Tests /CommandFactory

---
+++ Battleships/Battleships.UnitTests/Factory/CommandFactoryShould.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel;
+using Autofac;
+using Battleships.BattleShipsEngine;
+using Battleships.Factory;
+using Battleships.UnitTests.Factory;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Battleships.UnitTests.BattleshipsFactoryTests
+{
+    [TestClass]
+    public class CommandFactoryShould
+    {
+        [TestMethod]
+        public void SetFieldContainerToTheConstructorValuePassed()
+        {
+            //Arrange & Act
+            var stubContainer = new Mock<IComponentContext>(); 
+            var mockedCommandFactory = new MockedCommandFactory(stubContainer.Object);
+            //Assert
+            Assert.AreSame(mockedCommandFactory.Container, stubContainer.Object);
+        }
+
+        [TestMethod]
+        public void InvokeCreateMethod()
+        {
+            //Arrange & Act
+            var stubContainer = new Mock<IComponentContext>();
+            var mockedCommandFactory = new Mock<ICommandFactory>();
+            mockedCommandFactory.Setup(x => x.Create(It.IsAny<string>())).Returns(value: It.IsAny<ICommand>());
+            //Assert
+            //mockedCommandFactory.Create(cmName).Verify(x => x.Create(It.IsAny<string>()), Times.Once);
+            mockedCommandFactory.Verify(x => x.Create(It.IsAny<string>()), Times.Once);
+        }
+    }
+}
---
+++ Battleships/Battleships.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Battleships.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Battleships.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ca9603df-8e32-4b12-a241-4a062b28c44a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
