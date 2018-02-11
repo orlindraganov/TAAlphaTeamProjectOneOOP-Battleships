147fff9d8966ba5ccd67cf184e254cc7b54ae15e
cheficha <stefanzhekov95@gmail.com>
2018-02-11 17:38:18 +0200

    CreatedTests For Engine 1 Test for now !

---
+++ Battleships/Battleships.UnitTests/EngineTests/EngineShould.cs
@@ -0,0 +1,32 @@
+﻿using Battleships.BattleshipsEngine;
+using Battleships.BattleshipsEngine.Providers;
+using Battleships.BattleShipsEngine;
+using Battleships.BattleShipsEngine.Contracts;
+using Battleships.Factory;
+using Battleships.View.Contracts;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleships.UnitTests.EngineTests
+{
+    [TestClass]
+    public class EngineShould
+    {
+        [TestMethod]
+        public void StartsAtleastOnce()
+        {
+     
+            var engineMOck = new Mock<IEngine>();
+            engineMOck.Setup(x => x.Start()).Verifiable();
+            engineMOck.Object.Start();
+
+            engineMOck.Verify(x=>x.Start(),Times.AtLeastOnce);
+
+        }
+    }
+}
